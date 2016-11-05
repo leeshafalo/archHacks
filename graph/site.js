f5bb610a4b9b806c2201f885c6fc24a6e6941305
AmyDaYe <dlee57@hawk.iit.edu>
2016-11-05 03:31:36 -0500

    site.js for checkbox angularjs module

---
+++ graph/site.js
@@ -0,0 +1,12 @@
+var app = angular.module('myApp', ['ngMaterial'])
+
+.controller('AppCtrl', function($scope) {
+
+  $scope.data = {};
+  $scope.data.cb1 = true;
+  $scope.data.cb2 = false;
+  $scope.data.cb3 = false;
+  $scope.data.cb4 = false;
+  $scope.data.cb5 = false;
+
+});
